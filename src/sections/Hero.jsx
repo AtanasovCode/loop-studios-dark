eb7c7d3413c6dfd7c2182f7e6ae1bff1c6ff3d4b
AtanasovCode <a_ljupce@yahoo.com>
2024-09-20 14:52:54 +0200

    added hero image for mobile view

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,11 @@
+
+
+const Hero = () => {
+    return (
+        <div className="min-h-[100vh] w-full flex flex-col items-center justify-start">
+            <div className="absolute top-0 left-0 w-full h-full bg-hero-mobile bg-center bg-contain z-0"></div>
+        </div>
+    );
+}
+
+export default Hero;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,18 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {},
-      fontFamily: {},
+      colors: {
+        'dark-gray': "hsl(0, 0%, 55%)",
+        'very-dark-gray': "hsl(0, 0%, 41%)",
+      },
+      backgroundImage: {
+        'hero-mobile': "url('/src/assets/images/mobile/image-hero.jpg')",
+        'hero-desktop': "url('/src/assets/images/desktop/image-hero.jpg')",
+      },
+      fontFamily: {
+        'font-josefin': "Josefin Sans",
+        'font-alata': "Alata",
+      },
     },
   },
   plugins: [],
