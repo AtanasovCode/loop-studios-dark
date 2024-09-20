3d6f7a78f64116df645aba5f9fd817660bfd456f
AtanasovCode <a_ljupce@yahoo.com>
2024-09-20 22:30:29 +0200

    corrected font family in hero box component

--- src/components/HeroBox.jsx
+++ src/components/HeroBox.jsx
@@ -2,7 +2,7 @@
 
 const HeroBox = () => {
     return (
-        <div className="p-10 flex items-center justify-center text-white border-2 border-solid border-white w-[50%] uppercase
+        <div className="p-8 flex items-center font-font-josefin justify-center text-white border-2 border-solid border-white w-[55%] uppercase
         text-7xl font-light text-left mt-28">
             Immersive experiences that deliver
         </div>
