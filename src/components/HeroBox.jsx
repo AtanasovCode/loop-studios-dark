9828ede0a92ad71d3935ac0b04790d5e9c14a87c
AtanasovCode <a_ljupce@yahoo.com>
2024-09-21 19:26:34 +0200

    made hero fully responsive

--- src/components/HeroBox.jsx
+++ src/components/HeroBox.jsx
@@ -2,8 +2,12 @@
 
 const HeroBox = () => {
     return (
-        <div className="p-8 flex items-center font-font-josefin justify-center text-white border-2 border-solid border-white w-[55%] uppercase
-        text-7xl font-light text-left mt-28">
+        <div className="p-8 lg:p-8 flex items-center font-font-josefin justify-center text-white border-2 border-solid 
+        border-white uppercase
+        font-light text-left mt-28
+        w-[100%] xs:w-[85%] md:w-[70%] lg:w-[65%] xl:w-[55%] leading-[44px]
+        text-[2.5rem] md:text-5xl xl:text-6xl 2xl:text-7xl
+    ">
             Immersive experiences that deliver
         </div>
     );
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,11 +4,11 @@ import HeroBox from "../components/HeroBox";
 const Hero = () => {
     return (
         <div className="min-h-[89vh] w-full flex flex-col items-center justify-start relative">
-            <div className="relative z-50 flex flex-col items-start justify-start w-[75%]">
+            <div className="relative z-50 flex flex-col items-start justify-start w-[90%] lg:w-[75%]">
                 <Nav />
                 <HeroBox />
             </div>
-            <div className="absolute top-0 left-0 w-[100vw] h-[89vh] bg-tint z-20 flex items-center justify-center">
+            <div className="hidden lg:absolute top-0 left-0 w-[100vw] h-[89vh] bg-tint z-20 lg:flex items-center justify-center">
                 {/* div used for applying tint to hero image */}
             </div>
             <div className="absolute top-0 left-0 w-full h-full bg-hero-mobile bg-center bg-cover z-0 bg-no-repeat md:bg-hero-desktop">
