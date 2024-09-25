c9eca714cde6f201f82af75a8ba67ec65e808ef6
AtanasovCode <a_ljupce@yahoo.com>
2024-09-25 20:53:05 +0200

    fixed footer responsive issue

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -11,7 +11,7 @@ import twitter from '../assets/images/icon-twitter.svg';
 const Footer = () => {
     return (
         <div className="w-full bg-black px-6 py-12 text-white flex items-center justify-center mt-24">
-            <div className="flex flex-col items-center justify-center md:flex-row md:justify-between
+            <div className="flex flex-col items-center justify-center lg:flex-row md:justify-between
             w-[90%] xs:w-[70%] sm:w-[60%] md:w-[55%] lg:w-[90%] xl:w-[73%]">
                 <div className="flex flex-col items-center justify-center md:items-start">
                     <div className="flex items-center justfy-center w-36 mb-6">
@@ -29,7 +29,7 @@ const Footer = () => {
                         <FooterLink text="Support" />
                     </div>
                 </div>
-                <div className="flex flex-col items-center justify-center mt-6 md:items-end">
+                <div className="flex flex-col items-center justify-center mt-6 lg:items-end">
                     <div className="grid grid-cols-4 gap-4 justify-items-center">
                         <SocialIcon
                             icon={facebook}
